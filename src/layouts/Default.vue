8e0f16db747f28274a9c06901dc966ce5459b19c
gwenf <gwenfaraday@pm.me>
2020-07-22 12:11:00 -0400

    first commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-app>
+    <v-navigation-drawer width="240px" permanent>
+      <v-list-item>
+        <!-- <v-list-item-avatar>
+          <img src="https://randomuser.me/api/portraits/men/81.jpg">
+        </v-list-item-avatar> -->
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Gwendolyn Faraday
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Software Developer
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <v-container>
+        <slot/>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: 'About', icon: 'mdi-account-box-outline' },
+        { title: 'Projects', icon: 'mdi-apps' },
+        { title: 'Education', icon: 'mdi-school-outline' },
+        { title: 'Contact', icon: 'mdi-message-text-outline' },
+      ]
+    }
+  }
+}
+</script>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+.v-application--wrap {
+  flex-direction: row;
+}
+.v-main {
+  flex: 1 0;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <Layout>
+
+    <section>
+      <v-container>
+        <v-row>
+          <v-col sm="6">
+            <g-image alt="Example image" src="~/profile.png" width="400" />
+          </v-col>
+          <v-col sm="6">
+            <h1>Hi, I'm Gwen.</h1>
+            <p>I'm a software developer...</p>
+          </v-col>
+        </v-row>
+      </v-container>
+    </section>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
